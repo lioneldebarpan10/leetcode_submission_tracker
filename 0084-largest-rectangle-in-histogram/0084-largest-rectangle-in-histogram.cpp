61e89f68ea895b0292284d9134a527161fe38739
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2026-08-15 18:59:04 +0530

    Time: 10 ms (95.43%), Space: 79.4 MB (96.47%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        stack<int> stk;
         int n = heights.size();
-        stack<int>stk;
-        vector<int>leftSmaller(n);
-        vector<int>rightSmaller(n);
-
-        for(int i = 0 ; i < n ; i++){
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
-                stk.pop();
-            }
-            if(stk.empty()){
-                leftSmaller[i] = -1;
-            }
-            else{
-                leftSmaller[i] = stk.top();
-            }
-            stk.push(i);
-        }
-        while(!stk.empty()){
-            stk.pop();
-        }
-        for(int i = n - 1 ; i >= 0 ; i--){
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
-                stk.pop();
-            }
-            if(stk.empty()){
-                rightSmaller[i] = n;
-            }
-            else{
-                rightSmaller[i] = stk.top();
-            }
-            stk.push(i);
-        }
         int maxarea = 0;
-        for(int i = 0 ; i < n ; i++){
-            int width = rightSmaller[i] - leftSmaller[i] - 1;
-            int area = heights[i] * width;
-
-            maxarea = max(maxarea , area);
+        // Traverse all bars including one extra pass for cleanup
+        for(int i=0 ; i<=n ; i++){
+            // Process bars in stack while current bar is shorter
+            while(!stk.empty() && (i==n || heights[stk.top()] >= heights[i])){
+                int height = heights[stk.top()];
+                stk.pop();
+                // Calculate width for current height
+                int width;
+                if(stk.empty()){
+                    width = i;
+                }
+                else{
+                    width = i - stk.top() - 1;
+                }
+                // calculate maxarea
+                maxarea = max(maxarea , width * height);
+            }
+            // push curr index into the stack
+            
+            stk.push(i);
         }
         return maxarea;
+        
     }
 };
