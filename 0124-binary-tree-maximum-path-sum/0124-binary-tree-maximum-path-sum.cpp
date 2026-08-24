fe39f6d53212a59018948fb621acd80c4e7a93d0
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2026-08-24 23:14:05 +0530

    Time: 0 ms (100%), Space: 28 MB (20.57%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,22 +11,19 @@
  */
 class Solution {
 public:
-    int findmaxPathSum(TreeNode* root , int &maxi){
-        // if root is null then sum is 0
+    int findMaxPathSum(TreeNode* root , int &maxi){
         if(root == NULL) return 0;
-        // calculate maximum path sum of right and left sub tress
-        int leftpathsum = max(0, findmaxPathSum(root->left , maxi));
-        int rightpathsum = max(0, findmaxPathSum(root->right , maxi));
-        // maxi = max value of left + max value of right + current node value
-        maxi = max(maxi , leftpathsum + rightpathsum + root->val);
-        // Return the maximum sum considering
-        // only one branch (either left or right)
-        // along with the current node
-        return max(leftpathsum , rightpathsum) + root->val;
+
+        int leftPathSum = max(0 , findMaxPathSum(root->left , maxi));
+        int rightPathSum = max(0 , findMaxPathSum(root->right , maxi));
+
+        maxi = max(maxi , leftPathSum + rightPathSum + root->val);
+        return max(leftPathSum , rightPathSum) + root->val;
     }
+
     int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN; //initialize maxi as minimum possible integer value
-        findmaxPathSum(root , maxi); // call the recursive function to find maximum path sum
-        return maxi; // maximum path sum
+        int maxi = INT_MIN;
+        findMaxPathSum(root , maxi);
+        return maxi;
     }
 };
