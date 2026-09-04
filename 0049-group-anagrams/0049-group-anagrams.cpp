06751f1235dcf297e8696c4db801af851c74dcaf
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2026-09-04 16:45:59 +0530

    Time: 15 ms (69.5%), Space: 26.2 MB (42.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string , vector<string>>mpp;
+
         for(string s : strs){
             string key = s;
             sort(key.begin() , key.end());
             mpp[key].push_back(s);
         }
-        vector<vector<string>>result;
+
+        vector<vector<string>> res;
         for(auto mp : mpp){
-            result.push_back(mp.second);
+            res.push_back(mp.second);
         }
-        return result;
-        
+        return res;
     }
 };
