1d8dc3be3cc51fbeb524b7e0a87feab92a977b4d
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2026-09-13 02:09:43 +0530

    Time: 0 ms (100%), Space: 11.7 MB (71.91%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        stack<int>stk;
+
+        stk.push(-1);
+        int longest = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(s[i] == '('){
+                stk.push(i);
+            }
+            else{
+                stk.pop();
+                if(stk.empty()){
+                    stk.push(i);
+                }
+                else{
+                    longest = max(longest , i - stk.top());
+                }
+            }
+        }
+        return longest;
+    }
+};
