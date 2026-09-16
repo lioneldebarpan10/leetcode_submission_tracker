d14a786e94eccb5f44207fa10d1f0e7ec3a73e93
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2026-09-17 01:17:15 +0530

    Time: 22 ms (57.34%), Space: 79.5 MB (89.79%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> stk;
         int n = heights.size();
+        int height = 0;
+        int width = 0;
         int maxarea = 0;
-        // Traverse all bars including one extra pass for cleanup
-        for(int i=0 ; i<=n ; i++){
-            // Process bars in stack while current bar is shorter
-            while(!stk.empty() && (i==n || heights[stk.top()] >= heights[i])){
-                int height = heights[stk.top()];
+        stack<int> stk;
+
+        for(int i = 0 ; i <= n ; i++){
+            while(!stk.empty() && (i == n || heights[stk.top()] >= heights[i])){
+                height = heights[stk.top()];
                 stk.pop();
-                // Calculate width for current height
-                int width;
+
                 if(stk.empty()){
                     width = i;
                 }
                 else{
                     width = i - stk.top() - 1;
                 }
-                // calculate maxarea
-                maxarea = max(maxarea , width * height);
+
+                int area = height * width;
+                maxarea = max(maxarea , area);
             }
-            // push curr index into the stack
-            
-            stk.push(i);
+            if(i < n){
+                stk.push(i);
+            }
         }
         return maxarea;
-        
     }
 };
