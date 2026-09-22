8bf7f745f43be0a23f7b109e82c95bfdd1060077
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2026-09-23 04:14:13 +0530

    Time: 0 ms (100%), Space: 10.3 MB (95.7%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length();
+        vector<bool> dp(n + 1 , false);
+
+        dp[0] = true;
+        for(int i = 0 ; i <= n ; i++){
+
+            if(!dp[i]) continue;
+
+            for(string &word : wordDict){
+                int len = word.size();
+                if(i + len <= n && s.substr(i , len) == word){
+                    dp[i + len] = true; 
+                }
+            }
+        }
+        return dp[n];
+        
+    }
+};
